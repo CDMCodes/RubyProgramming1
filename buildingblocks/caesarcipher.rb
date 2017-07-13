ac36d75f653aba486313bdc7f07dd17544def6c8
Andrew Campbell <campbell.drew.m@gmail.com>
2017-07-13 07:01:49 -0400

    complete caesar cipher function

--- buildingblocks/caesarcipher.rb
+++ buildingblocks/caesarcipher.rb
@@ -1,21 +1,32 @@
 def caesar_cipher(sentence, shift)
-  #Create array of lowercase letters to shift through
-  #Create array of uppercase letters for same
+  #Create array of uppercase and lowercase letters to shift through
+  lowers = ("a".."z").to_a
+  uppers = ("A".."Z").to_a
+
+  #Create empty array for results, and split input sentence
   results = []
-  letters = sentence.split("")
-
-  #loop through letters
-    #if letter is a space (or any whitespace)
-      #write same whitespace to results array
-
-    #elsif letter is uppercase, use uppercase array
-      #find matching index of current letter
-      #increment index by shift factor - modulo to 26?
-      #assign new letter to results array at this index
-    #else use lowercase array
-      #do all of the same as above, but with other array
-
-    #join results array into a string
-
-
+  letters = sentence.split(//)
+  #puts letters
+  #loop through letters, shifting letters and just adding non-letters
+  letters.each do |l|
+    if (lowers.index(l) != nil)
+      start = lowers.index(l)
+      #puts "lowers at index #{start}"
+      stop = (start + shift) % 26
+      results << lowers[stop]
+    elsif (uppers.index(l) != nil)
+      start = uppers.index(l)
+      #puts "uppers at index #{start}"
+      stop = (start + shift) % 26
+      results << uppers[stop]
+    else
+      #puts "#{l} is not  a letter"
+      results << l
+    end
+  end
+results = results.join
+puts results
 end
+
+caesar_cipher("Test this",2)
+caesar_cipher("What a string!", 5)
