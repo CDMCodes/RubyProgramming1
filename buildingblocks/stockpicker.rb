123cf2b97b445142d0f47e06f6feb8637848de26
Andrew Campbell <campbell.drew.m@gmail.com>
2017-07-13 20:16:47 -0400

    first attempt at stockpicker...doesn't work

--- buildingblocks/stockpicker.rb
+++ buildingblocks/stockpicker.rb
@@ -1,5 +1,22 @@
 def stockpicker(daysarr)
-  daysarr.each do |dayprice|
-    
+
+  pick = {"low" => daysarr.sort[0],
+    "lowday" => nil,
+    "high" => daysarr.sort[-1],
+    "highday" => nil}
+
+  daysarr.each_with_index do |val, index|
+    #if number is smaller than initial, replace it
+    if val <= pick["low"]
+      pick["low"] = val
+      pick["lowday"] = index
+    else val >= pick["high"]
+      pick["high"] = val
+      pick["highday"] = index
+    end
   end
+  puts "Low day = #{pick["lowday"]},  High day = #{pick["highday"]}"
 end
+
+
+stockpicker([17,3,6,9,15,8,6,1,10])
