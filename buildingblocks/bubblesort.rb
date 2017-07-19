77ae581d5fa7857ae64857dcfc5f15c5652eed2d
Andrew Campbell <campbell.drew.m@gmail.com>
2017-07-19 07:16:40 -0400

    add sort_by using proc argument

--- buildingblocks/bubblesort.rb
+++ buildingblocks/bubblesort.rb
@@ -4,11 +4,11 @@ def bubble_sort (arr)
 # if A is bigger than B, switch their position
 # If not do nothing (next)
 # increment the index and loop
-puts(arr)
+#puts(arr)
   for i in 0..arr.length-1
     for j in 0..arr.length-2
       first = arr[j]
-      second = arr[j + 1] #this fails for final item in index
+      second = arr[j + 1]
       # puts first > second
       if first > second
         # puts first
@@ -18,12 +18,37 @@ puts(arr)
     end
   end
   puts(arr)
+  return(arr)
 end
 
 
-def bubble_sort_by(arr)
+def bubble_sort_by(arr, &compare)
+  for i in 0..arr.length-1
+    for j in 0..arr.length-2
+      first = arr[j]
+      second = arr[j + 1]
 
+      #we are assuming result of compare proc is negative, 0 or positive
+      comparison = compare.call(first, second) #does this work?
+
+      if comparison > 0
+        # puts first
+        arr[j] = second
+        arr[j + 1] = first
+      end
+    end
+  end
+  puts(arr)
+  return(arr)
 end
 
 
-bubble_sort([4,3,78,2,0,2])
+def proctest(arr, &blck)
+  arr.each_with_index{|el, indx| blck.call(el,indx)}
+end
+
+#bubble_sort([4,3,78,2,0,2])
+#puts proctest([4,3,78,2,0,2]) {|num, indx| puts "This #{indx} element contains #{num}"}
+bubble_sort_by(["hi","hello","hey"]) do |left, right|
+  left.length - right.length
+end
